00f0c1ad998664107e77fa0b1f4aa82d34ba2a5c
VISHWAS\vishwas <vishwas.s.patel@gmail.com>
2019-05-02 22:00:59 -0700

    Commit 5/2/2019

--- queues/RandomizedQueue.java
+++ queues/RandomizedQueue.java
@@ -4,14 +4,18 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdRandom;
+
 import java.util.Iterator;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
     private Item[] RandQueue;
-    private int noOfEle = 0;
+
+    private int noOfEle;
 
     public RandomizedQueue() {              // construct an empty randomized queue
-        RandQueue = (Item[]) new Object[noOfEle];
+        RandQueue = (Item[]) new Object[2];
+        noOfEle = 0;
     }
 
     public boolean isEmpty() {                 // is the randomized queue empty?
@@ -22,17 +26,88 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         return noOfEle;
     }
 
+    private void resize(int length) {
+        RandQueue = ()
+    }
+
     public void enqueue(Item item) {           // add the item
-        if ()
+        if (item == null)
+            throw new java.lang.IllegalArgumentException("item cannot be null");
+
+        if (noOfEle == RandQueue.length) {
+            resize(2 * RandQueue.length);
+        }
+        RandQueue[noOfEle] = item;
+        noOfEle++;
     }
 
     public Item dequeue() {                    // remove and return a random item
+        int random;
+        Item res;
+        Node temp;
+
+        if (noOfEle == 0) {
+            throw new java.util.NoSuchElementException("Randomized Queue is empty");
+        }
+        random = StdRandom.uniform(noOfEle);
+        Node iter = first;
+        temp = first;
+        while ((random > 0) && (iter != null)) {
+            random--;
+            temp = iter;
+            iter = iter.next;
+        }
+        res = iter.item;
+
+        if (iter.next == null) {
+            temp.next = null;
+            iter = null;
+        }
+        else {
+            temp.next = iter.next;
+            iter = null;
+        }
     }
 
     public Item sample() {                     // return a random item (but do not remove it)
+        int random;
+
+        if (noOfEle == 0) {
+            throw new java.util.NoSuchElementException("Randomized Queue is empty");
+        }
+        random = StdRandom.uniform(noOfEle);
+        Node temp = first;
+        while ((random > 0) && (temp != null)) {
+            random--;
+            temp = temp.next;
+        }
+        return temp.item;
     }
 
     public Iterator<Item> iterator() {        // return an independent iterator over items in random order
+        return new RandomizedQueueIterator();
+    }
+
+    private class RandomizedQueueIterator implements Iterator<Item> {
+        private int[] shuffleArray;
+
+        public RandomizedQueueIterator() {
+            shuffleArray = new int[noOfEle];
+            StdRandom.shuffle(shuffleArray);
+        }
+
+        public boolean hasNext() {
+
+        }
+
+        public Item next() {
+
+        }
+
+        public void remove() {
+            /* */
+            throw new java.lang.UnsupportedOperationException("Unsupported operation");
+        }
     }
 
     public static void main(String[] args) {   // unit testing (optional)
